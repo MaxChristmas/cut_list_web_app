2d667353d66406f69e460fc07b7da2b524e9ba29
Maxence NOEL <maxence.noel18@gmail.com>
2026-02-10 14:34:26 +0100

    Add language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  around_action :switch_locale
+
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    if I18n.available_locales.include?(locale)
+      session[:locale] = locale
+    end
+    redirect_back fallback_location: root_path
+  end
+
+  private
+
+  def switch_locale(&action)
+    locale = session[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
- resources :projects
-  
+  resources :projects
+
   devise_for :users
+
+  patch "locale", to: "application#set_locale", as: :locale
+
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   root "projects#index"
 end
 
