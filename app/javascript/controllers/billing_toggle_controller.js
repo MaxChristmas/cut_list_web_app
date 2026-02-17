a53eb212dd51392632c00e367481fe2d412f0d6c
Maxence NOEL <maxence.noel18@gmail.com>
2026-02-17 13:26:50 +0100

    Add one shot access

--- app/javascript/controllers/billing_toggle_controller.js
+++ app/javascript/controllers/billing_toggle_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["monthlyBtn", "yearlyBtn", "monthlyPrice", "yearlyPrice", "billingCycleInput"]
+  static targets = ["monthlyBtn", "yearlyBtn", "oneShotBtn", "monthlyPrice", "yearlyPrice", "subscriptionPlans", "oneShotPlans", "billingCycleInput"]
 
   connect() {
     this.cycle = "monthly"
@@ -18,21 +18,33 @@ export default class extends Controller {
     this.updateUI()
   }
 
+  selectOneShot() {
+    this.cycle = "one_shot"
+    this.updateUI()
+  }
+
   updateUI() {
     const isMonthly = this.cycle === "monthly"
+    const isYearly = this.cycle === "yearly"
+    const isOneShot = this.cycle === "one_shot"
 
-    this.monthlyBtnTarget.classList.toggle("bg-blue-600", isMonthly)
-    this.monthlyBtnTarget.classList.toggle("text-white", isMonthly)
-    this.monthlyBtnTarget.classList.toggle("bg-transparent", !isMonthly)
-    this.monthlyBtnTarget.classList.toggle("text-gray-400", !isMonthly)
-
-    this.yearlyBtnTarget.classList.toggle("bg-blue-600", !isMonthly)
-    this.yearlyBtnTarget.classList.toggle("text-white", !isMonthly)
-    this.yearlyBtnTarget.classList.toggle("bg-transparent", isMonthly)
-    this.yearlyBtnTarget.classList.toggle("text-gray-400", isMonthly)
+    this.#toggleBtn(this.monthlyBtnTarget, isMonthly)
+    this.#toggleBtn(this.yearlyBtnTarget, isYearly)
+    this.#toggleBtn(this.oneShotBtnTarget, isOneShot)
 
     this.monthlyPriceTargets.forEach(el => el.classList.toggle("hidden", !isMonthly))
-    this.yearlyPriceTargets.forEach(el => el.classList.toggle("hidden", isMonthly))
+    this.yearlyPriceTargets.forEach(el => el.classList.toggle("hidden", !isYearly))
+
+    this.subscriptionPlansTarget.classList.toggle("hidden", isOneShot)
+    this.oneShotPlansTarget.classList.toggle("hidden", !isOneShot)
+
     this.billingCycleInputTargets.forEach(input => input.value = this.cycle)
+  }
+
+  #toggleBtn(btn, active) {
+    btn.classList.toggle("bg-blue-600", active)
+    btn.classList.toggle("text-white", active)
+    btn.classList.toggle("bg-transparent", !active)
+    btn.classList.toggle("text-gray-400", !active)
   }
 }
