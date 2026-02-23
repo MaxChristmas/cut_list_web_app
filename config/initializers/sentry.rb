a6438398ac910ec9604d21ba2f06c28e4753b154
Maxence NOEL <maxence.noel18@gmail.com>
2026-02-23 12:22:01 +0100

    MISC - update env for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.dsn = ENV['SENTRY_DSN']
-  config.traces_sample_rate = 1.0
+if ENV['SENTRY_DSN'].present?
+  Sentry.init do |config|
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.dsn = ENV['SENTRY_DSN']
+    config.traces_sample_rate = 1.0
 
-  # Add data like request headers and IP for users,
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
-  config.send_default_pii = true
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+    config.send_default_pii = true
+  end
 end
