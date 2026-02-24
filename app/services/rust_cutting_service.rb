0dcf2a007cc797311df5a501c423a019c11a3645
Maxence NOEL <maxence.noel18@gmail.com>
2026-02-24 14:59:03 +0100

    update error message on panel

--- app/services/rust_cutting_service.rb
+++ app/services/rust_cutting_service.rb
@@ -3,6 +3,8 @@ require "net/http"
 class RustCuttingService
   OPTIMIZER_URL = ENV.fetch("OPTIMIZER_URL", "http://localhost:3001/optimize")
 
+  PIECE_DOES_NOT_FIT = /\bpiece (\d+x\d+) does not fit in stock (\d+x\d+)\b/
+
   def self.optimize(stock:, cuts:, kerf: 0, cut_direction: "auto", grain_direction: "none")
     payload = {
       stock: { length: stock[:l].to_i, width: stock[:w].to_i, grain: grain_direction },
@@ -28,9 +30,18 @@ class RustCuttingService
     response = http.request(request)
 
     unless response.is_a?(Net::HTTPSuccess)
-      raise "Optimizer returned #{response.code}: #{response.body}"
+      raise translate_error(response.body)
     end
 
     JSON.parse(response.body)
   end
+
+  def self.translate_error(body)
+    if (match = body.match(PIECE_DOES_NOT_FIT))
+      I18n.t("optimizer_errors.piece_does_not_fit", piece: match[1], stock: match[2])
+    else
+      I18n.t("optimizer_errors.generic", message: body)
+    end
+  end
+  private_class_method :translate_error
 end
